ec487df3ddea772326051c3dd13d6810faa5546a
JungAh <70677614+Jungahgo@users.noreply.github.com>
2024-05-20 01:55:34 +0900

    fix: goal insert, reward 조회 수정

--- src/main/java/com/widyu/healthcare/core/domain/domain/v1/GoalStatus.java
+++ src/main/java/com/widyu/healthcare/core/domain/domain/v1/GoalStatus.java
@@ -5,6 +5,8 @@ import lombok.*;
 
 import java.sql.Time;
 
+import static com.widyu.healthcare.support.config.AppConfig.GOAL_POINT;
+
 @Getter
 public class GoalStatus {
 
@@ -15,18 +17,20 @@ public class GoalStatus {
     private Time time;
     @NonNull
     private byte status;
+    @Nullable
     private Long pillNum;
+    @Nullable
     private String imgUrl;
     private long point;
     @Builder
-    public GoalStatus(long goalStatusIdx, long goalIdx, @NonNull Time time, Long pillNum, String imgUrl, long point) {
+    public GoalStatus(long goalStatusIdx, long goalIdx, @NonNull Time time, Long pillNum, String imgUrl) {
         this.goalStatusIdx = goalStatusIdx;
         this.goalIdx = goalIdx;
         this.time = time;
         this.status = (byte)0;
         this.pillNum = pillNum;
         this.imgUrl = imgUrl;
-        this.point = point;
+        this.point = GOAL_POINT;
     }
 
     public void setGoalIdx(long goalIdx){
